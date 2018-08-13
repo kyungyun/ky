cd77cf7fef4a4e3553faee108ac3cc93c5e47782
kyungyun <40787356+kyungyun@users.noreply.github.com>
2018-08-13 23:03:02 +0900

    Add files via upload

---
+++ p2p.js
@@ -0,0 +1,37 @@
+const WebSockets = require("ws");
+
+const sockets = [];
+
+const getSockets = () => sockets;
+
+const startP2PServer = server => {
+    const wsServer = new WebSockets.Server({ server });
+    wsServer.on("connection", ws => {
+        initSocketConnection(ws);
+        //console.log(`Hello Socket`);
+
+    });
+    console.log("Blockchain P2P Server running");
+};
+
+const initSocketConnection = socket => {
+    sockets.push(socket);
+    socket.on("message", (data) => {
+        console.log(data);
+    });
+    setTimeout(() => {
+        socket.send("welcome");
+    }, 5000);
+};
+
+const connectToPeers = newPeer => {
+    const ws = new WebSockets(newPeer);
+    ws.on("open", () => {
+        initSocketConnection(ws);
+    });
+};
+
+module.exports = {
+    startP2PServer,
+    connectToPeers
+};
